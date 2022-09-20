87b1d62496cf3a70297b848b0a9b113a7aedae7a
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-09-20 07:16:11 +0200

    Setup proxy config, environments and base Urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public forecasts?: WeatherForecast[];
+  private baseUrl = environment.baseUrl;
+
+  constructor(http: HttpClient) {
+    http
+      .get<WeatherForecast[]>(this.baseUrl + 'api/weatherforecast')
+      .subscribe(
+        (result) => {
+          this.forecasts = result;
+        },
+        (error) => console.error(error)
+      );
+  }
+
   title = 'HealthCheck';
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
