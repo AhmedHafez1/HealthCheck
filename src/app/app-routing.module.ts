8a737f86dee40ef6cfd85f15090866033c81948e
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2022-10-02 14:17:49 +0200

    added angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'health-check', component: HealthCheckComponent }
 ];
 
 @NgModule({
